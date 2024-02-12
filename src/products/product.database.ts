be43d74dc25d7b4bffaf3b3e6bd01a72703c7ff3
unknown <temporary1581@gmail.com>
2024-02-12 21:29:45 +0800

    Sencond commit

--- src/products/product.database.ts
+++ src/products/product.database.ts
@@ -1,23 +1,69 @@
-import {User, UnitUser, Users} from "../users/user.interface";
-import bcrypt from "bcryptjs"
+import {Product, Products, UnitProduct} from "./product.interface";
 import {v4 as random} from "uuid"
 import fs from "fs"
 
-let users: Users = loadUsers()
+let products: Products = loadProducts();
 
-function loadUsers () : Users {
+function loadProducts(): Products {
     try {
-        const data = fs.readFileSync("./users.json", "utf-8")
-        return JSON.parse(data)
+        const data = fs.readFileSync("./products.json", "utf-8");
+        return JSON.parse(data);
     } catch (error) {
-        
+        console.log(`Error ${error}`);
+        return {}
     }
 }
 
-function saveUsers () {
+function saveProducts() {
     try {
-        fs.writeFileSync("./users.json", JSON.stringify(users), "utf-8")
+        fs.writeFileSync("./products.json", JSON.stringify(products), "utf-8");
+        console.log(`Products saved successfully!`);
     } catch (error) {
-        
+        console.log(`Error: ${error}`);
     }
 }
+
+
+export const findAll = async (): Promise<UnitProduct[]> => Object.values(products);
+
+export const findOne = async (id: string): Promise<UnitProduct> => products[id];
+
+export const create = async (productInfo : Product) : Promise<null | UnitProduct> => {
+    
+    let id = random();
+
+    let product = await findOne(id);
+
+    while (product) {
+        id = random();
+        product = await findOne(id);
+    }
+
+    products[id] = {
+        id : id,
+        ...productInfo
+    }
+    
+    saveProducts()
+
+    return products[id]
+}
+
+export const remove = async (id : string) : Promise<null | void> => {
+
+    const product = await findOne(id)
+
+    if(!product){
+        return null
+    }
+
+    delete products[id]
+
+    saveProducts()
+}
+
+
+export function update(id: string, newProduct: any) {
+    throw new Error("Function not implemented.");
+}
+    
